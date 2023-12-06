12b6b2208ed21e0f9cd9cc7bad57478fadc64d7f
Aqdas Rehman <121805614+Aqua110@users.noreply.github.com>
2023-12-06 18:58:37 +0530

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-const menu_button = document.querySelector(".menu-button-container")
-const navbar = document.querySelector(".navbar")
-menu_button.addEventListener('click', () => {navbar.classList.toggle("active-menu")});
