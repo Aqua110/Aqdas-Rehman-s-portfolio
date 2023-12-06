2b7e13a807fbabc62cfeb7a0eb2bd3cacd7b10ed
Aqdas Rehman <121805614+Aqua110@users.noreply.github.com>
2023-12-06 18:25:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+const menu_button = document.querySelector(".menu-button-container")
+const navbar = document.querySelector(".navbar")
+menu_button.addEventListener('click', () => {navbar.classList.toggle("active-menu")});
