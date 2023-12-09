09e82e2c39cb65cc71c95df9a62d345557bc7390
Aqdas Rehman <121805614+Aqua110@users.noreply.github.com>
2023-12-09 21:12:00 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-const menu_button = document.querySelector(".menu-button-container")
-const navbar = document.querySelector(".navbar")
+// menu button js
+
+const menu_button = document.querySelector(".menu-button-container");
+const navbar = document.querySelector(".navbar");
 menu_button.addEventListener('click', () => {navbar.classList.toggle("active-menu")});
+
+// download resume js/
+
+const resume_button = document.querySelector("#download-resume");
+resume_button.addEventListener('click', () => {alert("The resume is not avilable yet...\nIt wiil be avilable soon...");});
